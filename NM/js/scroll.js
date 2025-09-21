647a2be86bc2af55c3d6d28fc989567561d2dd40
Ugom1 <ugomorin63@gmail.com>
2025-09-21 21:44:12 +0200

    [ADD] Télécharger

--- NM/js/scroll.js
+++ NM/js/scroll.js
@@ -1,17 +1,28 @@
 function closeLightbox() {
-    const currentScrollY = window.scrollY;
-    location.hash = '';
-    setTimeout(function() {
-        window.scrollTo(0, currentScrollY);
-    }, 10);
-}
-
-(function(){
-    document.addEventListener('keydown', function(e){
-        if (e.key === 'Escape' || e.key === 'Esc') {
-            if (location.hash && location.hash.indexOf('lb-') === 1) {
-                closeLightbox();
-            }
+    const currentScrollY = window.scrollY
+    location.hash = ""
+    setTimeout(() => {
+      window.scrollTo(0, currentScrollY)
+    }, 10)
+  }
+  ;(() => {
+    document.addEventListener("keydown", (e) => {
+      if (e.key === "Escape" || e.key === "Esc") {
+        if (location.hash && location.hash.indexOf("lb-") === 1) {
+          closeLightbox()
         }
-    });
-})();
+      }
+    })
+  })()
+  
+  function downloadImage(imageUrl, filename) {
+    const link = document.createElement("a")
+    link.href = imageUrl
+    link.download = filename
+    link.style.display = "none"
+  
+    document.body.appendChild(link)
+    link.click()
+    document.body.removeChild(link)
+  }
+  
